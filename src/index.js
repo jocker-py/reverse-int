3248dd56d9be2569bf029269d91d645b1509f8a1
jocker-py <buladzislau@gmail.com>
2022-01-06 00:15:55 +0300

    fix: fix code in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    n = String(Math.abs(n));
+    let i = n.length - 1;
+    let str = '';
+
+    while(i >= 0){
+        str += n[i];
+        i--;
+    }
+    return str;
 }
